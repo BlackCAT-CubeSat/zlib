3e8bcd37f382390ac5fd47c6fe59343ee14df1bb
Zachary Catlin <zec0@psu.edu>
2022-05-12 20:55:19 -0400

    Added missing function prototypes
    This fixes errors that appear when compiling with -Werror=strict-prototypes.

--- crc32.c
+++ crc32.c
@@ -114,6 +114,8 @@ local z_crc_t x2nmodp OF((z_off64_t n, unsigned k));
   instruction, if one is available. This assumes that word_t is either 32 bits
   or 64 bits.
  */
+local z_word_t byte_swap OF((z_word_t word));
+
 local z_word_t byte_swap(word)
     z_word_t word;
 {
@@ -714,6 +716,9 @@ unsigned long ZEXPORT crc32_z(crc, buf, len)
   least-significant byte of the word as the first byte of data, without any pre
   or post conditioning. This is used to combine the CRCs of each braid.
  */
+local z_crc_t crc_word OF((z_word_t data));
+local z_word_t crc_word_big OF((z_word_t data));
+
 local z_crc_t crc_word(data)
     z_word_t data;
 {
